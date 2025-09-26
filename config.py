7a9a4d95cdbeb6e7c6c97c784ef4610c43d442b5
mtvnnb <nmnguyen.9721@gmail.com>
2025-09-26 09:57:19 +0700

    update quiz URL to the latest version

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # --- General Configuration ---
-QUIZ_URL = "https://taplai.com/hoc-on-tap-ly-thuyet-600-cau.html"
+QUIZ_URL = "https://taplai.com/hoc-ly-thuyet-600-cau-lai-xe-o-to-truc-tuyen-moi-nhat.html"
 TOTAL_QUESTIONS = 600
 
 # --- File Paths ---
